b30764a2402b11c42c1dfd15a1bc81550eb89402
Rocket-Rabbit <good9moon@gmail.com>
2021-04-19 19:16:58 +0900

    4# map

--- src/Food.js
+++ src/Food.js
@@ -1,38 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 //import "./Movie.css";
-// import { Link } from 'react-router-dom';
 
-function Food({ title, year, summary, poster, genres, posterLg, back }) {
+function Food({ name }) {
   return (
     <div className="movie">
-      <Link to={{ pathname: '/movie-detail', state: { title, year, summary, poster, posterLg, genres, back } }}>
-      {/* state:에 파라미터를 받는다 */}
-      <img src={poster} alt={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="movie_genres">
-          {genres.map((genre, index) => {
-            return (
-              <li key={index} className="movie_genre">{genre}</li>
-              // map을 통해 반복되는 콤포넌트는 반드시 고유의 key 가 있어야 한다.
-            );
-          })}
-        </ul>
-        <p className="movie_summary">{summary.slice(0, 180)}...</p>
-      </div>
-      </Link>
+      <p>{name}</p>
     </div>
   );
 }
-
-Movie.propTypes = {
-  year: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
 
 export default Food;
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import XMLParser from 'react-xml-parser';
-// import Movie from './Movie';
+import Food from './Food';
+
 // import "./Home.css";
 
 // https://yts-proxy.now.sh/list_movies.json
@@ -9,19 +10,20 @@ import XMLParser from 'react-xml-parser';
 class Home extends React.Component {  
   state = {
     isLoading: true,
-    store: [],
+    dgList: [],
   }
 
   getMovies = async () => {
-    const movies = await axios.get('http://dgfc.or.kr/performance/xml/calenderMonth.asp?year=2021&month=04')
-    .then(function (response) {
-      const dgData = new XMLParser().parseFromString(response.data);
-      const dgShow = dgData.children[2].children.slice(1,12); 
+    await axios.get('http://dgfc.or.kr/performance/xml/calenderMonth.asp?year=2021&month=04').then((response) => {
+      // 화살표 함수로 바인딩.
+      const {
+        children: [ , , { children: exhibition } ]
+      } = new XMLParser().parseFromString(response.data);
+      const dgShow = exhibition.slice(1,11); 
       //dgShow.slice(1, 12);
-      console.log(dgShow);
-    });
-    //this.setState({ store: movies, isLoading: false });
-    // movies : name과 value가 동일한 이름일 때 단축속성 사용 가능, (= movies: movies)
+      this.setState({ dgList: dgShow, isLoading: false });
+      //console.log(this.state.dgList);
+    });    
   }
 
   componentDidMount() {
@@ -29,42 +31,31 @@ class Home extends React.Component {
   }
 
   render() {
-    const { isLoading, store } = this.state;
-    // state의 상태 변화를 감지하고, 감지되는 순간 다시 render() 실행.
+    const { isLoading, dgList } = this.state;
+    console.log(dgList);
     return(
-       <p>Hi~</p>
+      <section className="container">
+      { isLoading
+        ? (
+          <div className="loader">
+            <span className="loader_text">Loading</span>
+          </div>
+        ) : (
+          <div className="movies">
+            { dgList.map((evt, index) => 
+              (
+                <Food 
+                  key={index} 
+                  name={evt.children[1].value}
+                  place={evt.children[7].value}
+                />
+              ) 
+            )}
+          </div>
+        )}
+    </section>
     );
   }
 }
 
 export default Home;
-
-/* 
- <section className="container">
-          { isLoading 
-            ? (
-              <div className="loader">
-                <span className="loader_text">Loading</span>
-              </div>
-            ) : (
-          // console.log(movie);
-          // movies에는 api로 불러온 movies배열
-          // import된 Movie로 값을 넘겨주고 컴포넌트 반환 
-              <div className="movies">
-              { store.map(movie => (
-                <Movie
-                  key={movie.id}
-                  //id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  posterLg={movie.large_cover_image}
-                  genres={movie.genres}//장르(배열)
-                  back={movie.background_image}
-                />
-              )) }
-            </div>
-            )}
-        </section>
-        */
